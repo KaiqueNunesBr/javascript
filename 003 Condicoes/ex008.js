218e84ff4385a401526f646a7cbbadccd6b0b431
Francisco Kaique <kaique49nunes@gmail.com>
2023-12-05 18:44:12 -0300

    condicoes p1

---
+++ 003 Condicoes/ex008.js
@@ -0,0 +1,10 @@
+var pais = 'EUAa'
+if(pais == 'Brasil') {
+    console.log('Olá, você é brasileiro!')
+} else {
+    if (pais == 'EUA'){
+        console.log('Olá, você é americano!')
+        }else{
+            console.log(`Você é de ${pais}, mas não soube qual país você é`)
+        }
+}
